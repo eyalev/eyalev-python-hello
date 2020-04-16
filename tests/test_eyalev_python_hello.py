aac2f6ff495aa894d98832e587fea119ec22948d
Eyal Levin <eyalev@gmail.com>
2020-04-16 13:17:20 +0300

    Add 'Hello' print logic

--- tests/test_eyalev_python_hello.py
+++ tests/test_eyalev_python_hello.py
@@ -1,5 +1,13 @@
+
 from eyalev_python_hello import __version__
+from eyalev_python_hello import main
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_hello(capsys):
+    main()
+    captured = capsys.readouterr()
+    assert captured.out == "Hello\n"
